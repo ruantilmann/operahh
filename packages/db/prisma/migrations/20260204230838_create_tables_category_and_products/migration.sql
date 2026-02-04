917f3d181ecfdb2cb4985f0129662542b1ff9724
Ruan Carlos <ruan.tilmann@gmail.com>
2026-02-04 20:09:53 -0300

    Adicionado modelos de Category e Product com enum CategoryType e suas respectivas tabelas no banco de dados

---
+++ packages/db/prisma/migrations/20260204230838_create_tables_category_and_products/migration.sql
@@ -0,0 +1,37 @@
+-- CreateEnum
+CREATE TYPE "CategoryType" AS ENUM ('PRODUCT', 'STOCK');
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" "CategoryType" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DECIMAL(10,2) NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Category_type_idx" ON "Category"("type");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_type_name_key" ON "Category"("type", "name");
+
+-- CreateIndex
+CREATE INDEX "Product_categoryId_idx" ON "Product"("categoryId");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
